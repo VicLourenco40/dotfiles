2889fc58b31953dbd57869aff709c15d293dd8dc
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-07-09 12:19:40 +0100

    Add neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+vim.g.user42 = 'vde-albu'
+vim.g.mail42 = 'vde-albu@student.42porto.com'
+vim.g.mapleader = ' '
+vim.o.clipboard = 'unnamedplus'
+vim.o.undofile = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.scrolloff = 10
+vim.o.cindent = true
